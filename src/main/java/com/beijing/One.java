209ca923550c110935b9650e89892715a5f2e637
老郭 <528092955@qq.com>
2022-09-26 17:28:35 +0800

    项目经理最终操作2222

--- src/main/java/com/beijing/One.java
+++ src/main/java/com/beijing/One.java
@@ -2,6 +2,6 @@ package com.beijing;
 
 public class One {
     public static void main(String[] args) {
-        System.out.println("经理的第三次javaSE");
+        System.out.println("22222222222javaSE");
     }
 }
--- src/main/java/com/beijing/two.java
+++ src/main/java/com/beijing/two.java
@@ -2,6 +2,6 @@ package com.beijing;
 
 public class two {
     public static void main(String[] args) {
-        System.out.println("222222222222");
+        System.out.println("222222222222啊实打实");
     }
 }
